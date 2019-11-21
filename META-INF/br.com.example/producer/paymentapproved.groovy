db5e24ad1b6b4cf4ea57c48ed94df2d5d8131dea
Marcelo Cordeiro De Quadros <marcelo.quadros@livelo.com.br>
2019-11-20 23:33:30 -0300

    first commit

---
+++ META-INF/br.com.example/producer/paymentapproved.groovy
@@ -0,0 +1,15 @@
+import org.springframework.cloud.contract.spec.Contract
+
+Contract.make {
+    request {
+        method GET()
+        urlPath $(regex('/v1/payments/[0-9]/status'))
+    }
+    response {
+        status 200
+        body(approved: true)
+        headers {
+            contentType(applicationJson())
+        }
+    }
+}
